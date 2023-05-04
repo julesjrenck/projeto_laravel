e3e5eaa01ceb918b6c933a8e0e0a235c1600161e
Jules Renck <julesrenck@gmail.com>
2023-05-04 11:51:50 -0300

    Alterado layout das mensagens de validação pra ser campo a campo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,15 +7,6 @@
 <body>
     <div class="container">
         <h1>{{ $title }}</h1>
-        @if (count($errors) > 0)
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         {{ $slot }}
     </div>
 </body>
--- resources/views/components/users/form.blade.php
+++ resources/views/components/users/form.blade.php
@@ -5,27 +5,69 @@
     @endisset
     <div class="col-md-4">
         <label for="nome" class="form-label">Nome Completo:</label>
-        <input type="text" id="nome" name="nome" class="form-control" @isset($nome)value="{{ $nome }}"@endisset>
+        <input type="text" id="nome" name="nome" class="form-control
+            @error('nome') is-invalid @enderror" 
+            @isset($nome)value="{{ $nome }}"@endisset>
+        @error('nome')  
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror  
     </div>    
     <div class="col-md-4">
         <label for="senha" class="form-label">Senha:</label>
-        <input type="password" id="senha" name="senha" class="form-control" @isset($senha)value="{{ $senha }}"@endisset>
+        <input type="password" id="senha" name="senha" class="form-control
+            @error('senha') is-invalid @enderror"  
+            @isset($senha)value="{{ $senha }}"@endisset>
+        @error('senha')  
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror  
     </div>
     <div class="col-md-4">
         <label for="data_nascimento" class="form-label">Data de Nascimento:</label>
-        <input type="date" id="data_nascimento" name="data_nascimento" class="form-control" @isset($datanascimento)value="{{ $datanascimento }}"@endisset>
+        <input type="date" id="data_nascimento" name="data_nascimento" class="form-control 
+            @error('data_nascimento') is-invalid @enderror"
+            @isset($datanascimento)value="{{ $datanascimento }}"@endisset>
+        @error('data_nascimento')  
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror      
     </div>
     <div class="col-md-4">
         <label for="cpf" class="form-label">CPF:</label>
-        <input type="text" id="cpf" name="cpf" class="form-control" @isset($cpf)value="{{ $cpf }}"@endisset>
+        <input type="text" id="cpf" name="cpf" class="form-control
+            @error('cpf') is-invalid @enderror"
+            @isset($cpf)value="{{ $cpf }}"@endisset>
+        @error('cpf')  
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror        
     </div>
     <div class="col-md-4">
         <label for="telefone" class="form-label">Telefone:</label>
-        <input type="tel" id="telefone" name="telefone" class="form-control" @isset($telefone)value="{{ $telefone }}"@endisset>
+        <input type="tel" id="telefone" name="telefone" class="form-control
+            @error('telefone') is-invalid @enderror" 
+            @isset($telefone)value="{{ $telefone }}"@endisset>
+        @error('telefone')  
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror        
     </div>
     <div class="col-md-4">
         <label for="email" class="form-label">E-mail:</label>
-        <input type="email" id="email" name="email" class="form-control" @isset($email)value="{{ $email }}"@endisset>
+        <input type="email" id="email" name="email" class="form-control
+            @error('email') is-invalid @enderror"  
+            @isset($email)value="{{ $email }}"@endisset>
+        @error('email')  
+        <div class="invalid-feedback">
+            {{$message}}
+        </div>
+        @enderror        
     </div>
     <!--div class="col-md-4">
         <label for="cep" class="form-label">CEP:</label>
